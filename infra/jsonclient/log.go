ddc0343d6929756de7c69d4e8b708a07cca260f6
Kutta Srinivasan <kutta@userclouds.com>
2022-11-21 19:47:36 -0700

    fix logError signature to match userclouds repo

--- infra/jsonclient/log.go
+++ infra/jsonclient/log.go
@@ -2,6 +2,6 @@ package jsonclient
 
 import "context"
 
-func logError(ctx context.Context, method, url, errorMsg string, code int) {
+func logError(ctx context.Context, dontLog bool, method, url, errorMsg string, code int) {
 	// don't log by default in SDK
 }
