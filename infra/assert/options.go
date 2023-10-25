2afe5a973da6f6a62008e49278632c030ae9e8cd
uc-build-user <116039615+uc-build-user@users.noreply.github.com>
2023-10-25 11:03:52 -0700

    Sync monorepo state at "DenyAll access policy and a few other simple fixes" (#51)
    Syncing from
    userclouds/userclouds@eda6ce370c522f99300280cc51676cb2a9ef549f

--- infra/assert/options.go
+++ infra/assert/options.go
@@ -2,6 +2,7 @@ package assert
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/google/go-cmp/cmp"
 )
@@ -52,4 +53,20 @@ func Diff() Option {
 	return optFunc(func(os *options) {
 		os.diff = true
 	})
+}
+
+// CmpIgnoreTimes is a cmp.Option that ignores BaseModel.Created, BaseModel.Updated, and BaseModel.Version
+// This is useful if you want to display a diff without times (eg. `assert.Errorf("diff: %s", cmp.Diff(got, want, IgnoreTimes()))“)
+func CmpIgnoreTimes() cmp.Option {
+	return cmp.FilterPath(func(p cmp.Path) bool {
+		return strings.Contains(p.GoString(), "BaseModel.Created") ||
+			strings.Contains(p.GoString(), "BaseModel.Updated") ||
+			strings.Contains(p.GoString(), "BaseModel.Version")
+	}, cmp.Ignore())
+}
+
+// IgnoreTimes ignores BaseModel.Created, BaseModel.Updated, and BaseModel.Version for cmp operations
+// This is a helper to pass directly into eg. `assert.Equal`
+func IgnoreTimes() Option {
+	return CmpOpt(CmpIgnoreTimes())
 }
