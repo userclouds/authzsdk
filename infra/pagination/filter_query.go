9ea65edd4c319b2b6c8843522a3e8d60d5866d3e
UC Build User <infra+github-build-user@userclouds.com>
2024-02-16 22:19:54 +0000

    Sync monorepo state at b85abe3ac0510e9bd35d0985f5a961b2d6feba9c

--- infra/pagination/filter_query.go
+++ infra/pagination/filter_query.go
@@ -1,19 +1,6 @@
 package pagination
 
-import (
-	"regexp"
-	"strings"
-
-	"userclouds.com/infra/ucerr"
-)
-
-type nodeType string
-
-const (
-	composite nodeType = "composite"
-	leaf      nodeType = "leaf"
-	nested    nodeType = "nested"
-)
+const nested = 1
 
 type operator string
 
@@ -33,46 +20,30 @@ const (
 )
 
 func (o operator) isArrayOperator() bool {
-	switch o {
-	case has:
-	default:
-		return false
-	}
-
-	return true
+	return o == has
 }
 
 func (o operator) isComparisonOperator() bool {
 	switch o {
-	case eq:
-	case ge:
-	case gt:
-	case le:
-	case lt:
-	case ne:
-	default:
-		return false
+	case eq, ge, gt, le, lt, ne:
+		return true
 	}
 
-	return true
+	return false
 }
 
 func (o operator) isPatternOperator() bool {
 	switch o {
-	case il:
-	case lk:
-	case nl:
-	default:
-		return false
+	case il, lk, nl:
+		return true
 	}
 
-	return true
+	return false
 }
 
 func (o operator) isLogicalOperator() bool {
 	switch o {
-	case and:
-	case or:
+	case and, or:
 	default:
 		return false
 	}
@@ -117,116 +88,54 @@ func (o operator) queryString() string {
 
 // FilterQuery represents a parsed query tree for a filter query
 type FilterQuery struct {
-	nodeType      nodeType
-	key           string
-	value         string
-	queryOperator operator
-	leftQuery     *FilterQuery
-	rightQuery    *FilterQuery
+	value      any
+	parent     *FilterQuery
+	leftChild  *FilterQuery
+	rightChild *FilterQuery
 }
 
-var leafFilter = regexp.MustCompile(`^[(]'[^']+',[A-Z]+,'.*?[^\\]'[)]`)
-
-func parseLeafQuery(s string) (*FilterQuery, string, error) {
-	// s must be of the form: ('key',op,'value')...
-	if !leafFilter.MatchString(s) {
-		return nil, "", ucerr.Errorf("query '%s' is not of the form '('key',operator,'value')", s)
+func (fq *FilterQuery) addChild(value any) *FilterQuery {
+	if fq.leftChild == nil {
+		fq.leftChild = &FilterQuery{
+			value:  value,
+			parent: fq,
+		}
+		return fq.leftChild
 	}
-
-	leafPrefix := leafFilter.FindString(s)
-	remainder := strings.TrimPrefix(s, leafPrefix)
-
-	leafParts := strings.SplitN(strings.TrimPrefix(leafPrefix, "('"), ",", 3)
-	if len(leafParts) != 3 {
-		return nil, "", ucerr.Errorf("query '%s' has %d parts, expected 3", s, len(leafParts))
+	fq.rightChild = &FilterQuery{
+		value:  value,
+		parent: fq,
 	}
-
-	key := strings.TrimSuffix(leafParts[0], "'")
-	op := operator(leafParts[1])
-	value := strings.TrimSuffix(strings.TrimPrefix(leafParts[2], "'"), "')")
-
-	if !op.isLeafOperator() {
-		return nil, "", ucerr.Errorf("query '%s' operator '%v' is not a leaf operator", s, op)
-	}
-
-	return &FilterQuery{
-		nodeType:      leaf,
-		key:           key,
-		queryOperator: op,
-		value:         value,
-	}, remainder, nil
+	return fq.rightChild
 }
 
-func parseFilterQuery(s string) (*FilterQuery, string, error) {
-	if strings.HasPrefix(s, "('") {
-		// we expect s is a leaf query
-		return parseLeafQuery(s)
+func (fq *FilterQuery) findAncestor() *FilterQuery {
+	for !fq.isRoot() {
+		fq = fq.parent
+		if fq.isNested() {
+			return fq
+		}
 	}
 
-	// query must either be nested or composite
-	if !strings.HasPrefix(s, "((") {
-		return nil, "", ucerr.Errorf("query '%s' is neither a nested nor composite query", s)
-	}
-
-	leftQuery, remainder, err := parseFilterQuery(strings.TrimPrefix(s, "("))
-	if err != nil {
-		return nil, "", ucerr.Errorf("could not parse left-most query of left clause '%s': '%v'", s, err)
-	}
-
-	if strings.HasPrefix(remainder, ")") {
-		// s was a nested query
-		//
-		// '((leftQuery))'
-		return &FilterQuery{
-			nodeType:  nested,
-			leftQuery: leftQuery,
-		}, strings.TrimPrefix(remainder, ")"), nil
-	}
-
-	if !strings.HasPrefix(remainder, ",") {
-		return nil, "", ucerr.Errorf("query '%s' is not of the form (leftQuery,operator,rightQuery)", s)
-	}
-
-	// we expect s was a composite query
-	//
-	// '((leftQuery),operator,(rightQuery))'
-	compositeParts := strings.SplitN(remainder, ",", 3)
-	if len(compositeParts) != 3 {
-		return nil, "", ucerr.Errorf("query '%s' had %d parts, expected 3", s, len(compositeParts))
-	}
-
-	op := operator(compositeParts[1])
-	if !op.isLogicalOperator() {
-		return nil, "", ucerr.Errorf("query '%s' operator '%v' is not a logical operator", s, op)
-	}
-
-	rightQuery, remainder, err := parseFilterQuery(compositeParts[2])
-	if err != nil {
-		return nil, "", ucerr.Errorf("could not parse right-most query of left clause '%s': '%v'", s, err)
-	}
-
-	if !strings.HasPrefix(remainder, ")") {
-		return nil, "", ucerr.Errorf("composite query '%s' does not end in ')'", s)
-	}
-
-	return &FilterQuery{
-		nodeType:      composite,
-		leftQuery:     leftQuery,
-		queryOperator: op,
-		rightQuery:    rightQuery,
-	}, strings.TrimPrefix(remainder, ")"), nil
+	return fq
 }
 
-// CreateFilterQuery creates a parsed filter query from a filter string
-func CreateFilterQuery(s string) (*FilterQuery, error) {
-	fq, remainder, err := parseFilterQuery(s)
-	if err != nil {
-		return nil, ucerr.Wrap(err)
-	}
+func (fq *FilterQuery) isNested() bool {
+	return fq.value == nested
+}
 
-	if remainder != "" {
-		return nil, ucerr.Errorf("filter query '%s' has unparsed remainder '%s'", s, remainder)
-	}
+func (fq *FilterQuery) isRoot() bool {
+	return fq.parent == nil
+}
 
-	return fq, nil
+func (fq *FilterQuery) rotateLeft(value any) {
+	leftChild := &FilterQuery{
+		value:      fq.value,
+		parent:     fq,
+		leftChild:  fq.leftChild,
+		rightChild: fq.rightChild,
+	}
+	fq.value = value
+	fq.leftChild = leftChild
+	fq.rightChild = nil
 }
